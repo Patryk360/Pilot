30f58931c11867aa0537e6b941ff13c79fd371c6
Patryk360 <nexigamingyt@gmail.com>
2023-08-08 13:32:07 +0200

    fix

--- Pilot/MainActivity.cs
+++ Pilot/MainActivity.cs
@@ -1,20 +1,33 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
-using System.Net.WebSockets;
 
 namespace Pilot
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private WebSocket ws;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.click_me);
+            button.Click += Button_Click;
+            
+            ws = new WebSocket(this, this);
+            ws.ConnectionClosed += (sender, args) =>
+            {
+                RunOnUiThread(() => {
+                    Toast.MakeText(this, "Połączenie zostało zamknięte", ToastLength.Short).Show();
+                });
+            };
+        }
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            ws.SendMessage("hejka");
         }
     }
 }
---
+++ Pilot/WebSocket.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using WebSocketSharp;
+
+namespace Pilot
+{
+    public class WebSocket
+    {
+        public event EventHandler ConnectionClosed;
+        private Context context;
+        private Activity activity;
+        private WebSocketSharp.WebSocket webSocket;
+        public WebSocket(Context context, Activity activity)
+        {
+            this.context = context;
+            this.activity = activity;
+            Start();
+        }
+        public void Start()
+        {
+            Task.Run(() => {
+                InitializeWebSocket();
+            });
+        }
+        private void InitializeWebSocket()
+        {
+            webSocket = new WebSocketSharp.WebSocket("ws://192.168.0.3:8080");
+            webSocket.WaitTime = TimeSpan.FromSeconds(3);
+            webSocket.OnMessage += WebSocket_OnMessage;
+            webSocket.OnError += WebSocket_OnError;
+            webSocket.OnClose += WebSocket_OnClose;
+
+            webSocket.Connect();
+        }
+        private void WebSocket_OnMessage(object sender, MessageEventArgs e)
+        {
+            activity.RunOnUiThread(() => {
+                ShowToast(e.Data);
+            });
+            Debug.WriteLine("Received message: " + e.Data);
+        }
+        private void WebSocket_OnError(object sender, ErrorEventArgs e)
+        {
+            Close();
+            Debug.WriteLine("Error: " + e.Message);
+        }
+        private void WebSocket_OnClose(object sender, CloseEventArgs e)
+        {
+            ConnectionClosed?.Invoke(this, EventArgs.Empty);
+        }
+        private void ShowToast(string message)
+        {
+            Toast.MakeText(context, message, ToastLength.Short).Show();
+        }
+        public void SendMessage(string message)
+        {
+            if (webSocket.IsAlive)
+            {
+                ShowToast("send message");
+                webSocket.Send(message);
+            }
+            else
+            {
+                ShowToast("connection error");
+            }
+        }
+        public void Close()
+        {
+            if (webSocket.IsAlive)
+            {
+                webSocket.Close();
+            }
+        }
+    }
+}
